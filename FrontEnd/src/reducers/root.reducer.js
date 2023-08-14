b9ed3b8feee5428a95f91cf823c82317c046bec1
devGangz <121560473+devGangz@users.noreply.github.com>
2023-08-14 19:07:04 +0700

    Delete FrontEnd directory

--- FrontEnd/src/reducers/root.reducer.js
+++
@@ -1,8 +0,0 @@
-// Tao Root reducer (chua reducer chinh)
-import { combineReducers } from "redux"
-import ReducerOne from "./24h.reducer"
-
-const rootReducer = combineReducers({
-    ReducerOne
-})
-export default rootReducer
